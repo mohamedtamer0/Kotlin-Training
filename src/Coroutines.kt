ba01094c53df30242b06e8e7910a5bf1b993b6bc
mohamed tamer0 <mt4452242@gmail.com>
2020-12-19 18:00:19 +0200

    last update .

--- src/Coroutines.kt
+++ src/Coroutines.kt
@@ -1,3 +1,16 @@
+import kotlin.concurrent.thread
+
+
 fun main(args: Array<String>) {
+    thread(start = true, name = "Another thread") {
+        (0..10).forEach{
+           println ("Message #$it  from ${Thread.currentThread().name}")
+        }
+    }
+
+    (0..10).forEach{
+        println ("Message #$it  from ${Thread.currentThread().name}")
+    }
+
 
 }
