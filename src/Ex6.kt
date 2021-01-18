a2ca5235a9ca6d288840cd9957b09ba6c5b7d754
mohamed tamer0 <mt4452242@gmail.com>
2021-01-18 03:20:46 +0200

    last update .

---
+++ src/Ex6.kt
@@ -0,0 +1,29 @@
+
+
+fun main(args: Array<String>) {
+
+
+    //While
+    var i : Int =0
+
+    while (i < 10)
+    {
+        println(i)
+        i++
+    }
+
+
+
+    //Do While
+    var x : Int =0
+    do
+    {
+        println(x)
+        x++
+    }
+    while (x < 10)
+
+
+
+
+}
---
+++ src/Ex8.kt
@@ -0,0 +1,37 @@
+fun main(args: Array<String>) {
+
+    var arr1 = IntArray(3)
+    arr1[0] = 4
+    arr1[1] = 6
+    arr1[2] = 9
+
+    println(arr1[1])
+    var total : Int = 0
+    for (x : Int in 0 until  arr1.size)
+    {
+        total = total + arr1[x]
+    }
+    println("Total = $total")
+
+
+
+
+
+    var arr2 = Array(3){""}
+    arr2[0] = "tt"
+    arr2[1] = "sdf"
+    arr2[2] = "sdf"
+
+    println(arr2[1])
+
+
+    for (i : Int in 0 until 3) {
+        println(arr2[i])
+    }
+
+
+
+
+
+
+}
