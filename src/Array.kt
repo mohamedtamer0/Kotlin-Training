9ae72aa5eb01fbff2d2ca96b0943738b8b47ee33
mohamed tamer0 <mt4452242@gmail.com>
2021-01-28 00:33:32 +0200

    last update .

---
+++ src/Array.kt
@@ -0,0 +1,35 @@
+fun main(args: Array<String>) {
+
+
+    val names = arrayOf("Tamer","Mohamed", "Ahmed")
+    println(names[0])
+    println(names[1])
+    println(names[2])
+
+    val numbers = arrayOf(565,6456, 5464)
+    println(numbers[0])
+    println(numbers[1])
+    println(numbers[2])
+
+
+    val arr = arrayOf(true,34, "Ahmed")
+    println(arr[0])
+    println(arr[1])
+    println(arr[2])
+
+
+    val str = Array<String>(3){""}
+    val c = CharArray(5)
+    val i = IntArray(9)
+
+    println(str.count())
+    println(c.count())
+    println(i.count())
+
+
+
+
+
+
+
+}
