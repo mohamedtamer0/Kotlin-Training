9ab9febfca1ba5a3faf1435aa0620cd1c7aefb68
mohamed tamer0 <mt4452242@gmail.com>
2021-01-28 00:50:43 +0200

    last update .

--- src/Array.kt
+++ src/Array.kt
@@ -18,18 +18,68 @@ fun main(args: Array<String>) {
     println(arr[2])
 
 
-    val str = Array<String>(3){""}
-    val c = CharArray(5)
+    val f = CharArray(5)
     val i = IntArray(9)
 
-    println(str.count())
-    println(c.count())
+    println(f.count())
     println(i.count())
 
 
+    println("======================================")
+
+
+    //Array With Looping
+    val names2 = arrayOf("Tamer","Mohamed", "Ahmed",565,6456, 5464)
+
+    var x =0
+    while (x < names2.count()) {
+        println(names2[x])
+        x++
+    }
 
 
 
+
+    val names3 = arrayOf("Tamer","Mohamed", "Ahmed",565,6456, 5464)
+
+    for (str in names3) {
+        println(str)
+    }
+
+
+    println("======================================")
+
+    //Two D Array
+
+    val row1 = arrayOf("Tamer","Mohamed", "Ahmed",2342,87889, 5464)
+    val row2 = arrayOf("Omar","Mohamed", "Ahmed",56765,6456, 675)
+    val row3 = arrayOf("Osama","Mohamed", "Ahmed",2341,6456, 7867)
+
+
+    val table = arrayOf(row1,row2,row3)
+
+    var r = 0
+    while (r < table.count()){
+
+        var c = 0
+        while (c < table[r].count()) {
+            print("${table[r][c]}" + " ; ")
+            c++
+        }
+        println()
+        r++
+    }
+
+
+    println("======================================")
+
+
+    for (row in table) {
+        for (col in row) {
+            print("$col" + " - ")
+        }
+        println()
+    }
 
 
 }
