89c3f831d4e20ddb025c270f8d0868730d650855
mohamed tamer0 <mt4452242@gmail.com>
2021-02-02 23:23:11 +0200

    last update .

--- src/PersonCL.kt
+++ src/PersonCL.kt
@@ -1,17 +1,27 @@
-class PersonCL(val firstName: String, val lastName:String) {
+class PersonCL(val firstName: String = "Peter", val lastName:String="Parker") {
 
+    var nickName:String? = null
 
-    init {
-        println("init 1")
+    set(value) {
+        field = value
+        println("the new nickName is $value")
     }
 
-    constructor() : this("Peter", "Parker") {
-        println("Secondary constructor")
-    }
 
-    init {
-        println("init 2")
-    }
+
+
+//    init {
+//        println("init 1")
+//    }
+//
+//    constructor() : this("Peter", "Parker") {
+//        println("Secondary constructor")
+//    }
+//
+//    init {
+//        println("init 2")
+//    }
+
 
 }
 
@@ -20,4 +30,6 @@ fun main(args: Array<String>) {
     val per = PersonCL()
     per.lastName
     per.firstName
+    per.nickName = "Man"
+    per.nickName = "New NickName"
 }
