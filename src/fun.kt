92ee8f2196d4c0a8322ca8fb0908a6603c28b535
mohamed tamer0 <mt4452242@gmail.com>
2021-02-02 22:54:30 +0200

    last update .

--- src/fun.kt
+++ src/fun.kt
@@ -5,7 +5,11 @@ fun sayHello(greeting:String, vararg itemsToGreet: String) {
     }
 }
 
+fun greetperson(greeting: String = "Hello", name:String = "Kotlin") = println("$greeting $name")
+
+
 fun main(args: Array<String>) {
     val arr = arrayOf("Kotlin", "Java")
     sayHello("Hi", *arr )
+    greetperson(name = "Tamer")
 }
