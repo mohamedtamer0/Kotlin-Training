f9047259ac14e2448372cc05ed35b7ab5d59802b
mohamed tamer0 <mt4452242@gmail.com>
2021-02-19 20:35:59 +0200

    last update .

---
+++ src/Tuple.kt
@@ -0,0 +1,12 @@
+
+
+fun myFun(name:String):Pair<String,Int> {
+    return Pair("Tamer",23)
+}
+
+
+fun main(args: Array<String>) {
+    val myWord = myFun("Tamer")
+    println(myWord.first)
+    println(myWord.second)
+}
