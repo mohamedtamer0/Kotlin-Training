a3e235de9775f81147ca7f3e4db6ab8538289d4a
mohamed tamer0 <mt4452242@gmail.com>
2021-02-19 23:11:08 +0200

    last update .

---
+++ src/Gen.kt
@@ -0,0 +1,22 @@
+
+
+fun <T: Comparable<T>> compareFun(input1: T, input2:T): T {
+
+    val res = input1.compareTo(input2)
+    return if (res > 0 ) input1 else input2
+
+}
+
+
+fun main(args: Array<String>) {
+    val int = compareFun(1,2)
+    val string = compareFun("Mohamed","Tamer")
+    val float = compareFun(12f,14f)
+    val long = compareFun(4L,2L)
+
+
+    println(int)
+    println(float)
+    println(string)
+    println(long)
+}
