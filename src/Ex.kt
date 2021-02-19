e3bf1aa6aa7d19fa636c2d66458050225baff2ef
mohamed tamer0 <mt4452242@gmail.com>
2021-02-19 21:06:05 +0200

    last update .

--- src/Ex.kt
+++ src/Ex.kt
@@ -1,3 +1,33 @@
-class Ex : Throwable(){
+import java.sql.Struct
+
+abstract class Google {
+    val name = "Tamer"
+    val age = 23
+    val isAvail = true
+
+    fun myFun():String {
+        return name
+    }
+
+    abstract fun myAge()
+    fun  is_Avail():String{
+        return isAvail.toString()
+    }
+
 }
 
+
+class Facebook:Google() {
+    override fun myAge() {
+        println("This is Facebook class.")
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val facebook = Facebook()
+    facebook.is_Avail()
+    facebook.myAge()
+    facebook.myFun()
+
+}
